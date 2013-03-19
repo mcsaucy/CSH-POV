ae187813ede494869c670ca73918a91ac9279491
Josh McSavaney <mcsaucy@csh.rit.edu>
2013-03-19 15:53:46 -0400

    Removed a spinlock in the input ISR. As a result, input echo-ing post-carriage return has been disabled.

--- input.h
+++ input.h
@@ -3,25 +3,27 @@
 
 #include <avr/interrupt.h>
 
-//looking at this 6 mongth later, having a busy wait in an ISR was a HORRIBLE idea
+//looking at this 6 months later, having a busy wait in an ISR was a HORRIBLE idea
 ISR(USART_RX_vect)
 {
-        uint8_t rx;
-        rx  = UDR0;
-        msg[i] = rx;
-        if (msg[i] == '\n' || msg[i] == '\r' || i + 1 == SIZE || msg[i] == '\0')
+    uint8_t rx;
+    rx = UDR0;
+    msg[i] = rx;
+    if (msg[i] == '\n' || msg[i] == '\r' || i + 1 == SIZE || msg[i] == '\0')
+    {
+#ifdef BREAK_EVERYTHING_TO_ECHO
+        for(uint8_t j = 0; j < i; j++)
         {
-                for(uint8_t j = 0; j < i; j++)
-                {
-			//this was pretty much my first ever ISR
-                        while (!(UCSR0A & (1 << UDRE0)));
-			//now that everything is screwed timing wise, continue
-                        UDR0 = msg[j];
-                }
-                i = 0;
-                UCSR0B &= ~(1 << RXEN0);
-	}
-	else i++;
+//this was pretty much my first ever ISR
+            while (!(UCSR0A & (1 << UDRE0)));
+//now that everything is screwed timing wise, continue
+            UDR0 = msg[j];
+        }
+#endif
+        i = 0;
+        UCSR0B &= ~(1 << RXEN0);
+    }
+    else i++;
 }
 
 #endif
